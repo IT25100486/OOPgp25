f5305086c915c5d298ca677d7608f3c742104784
IT25100486 <it25100486@my.sliit.lk>
2026-04-22 10:50:06 +0530

    Delete src directory

--- src/main/java/Repository/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package Repository;
-
-import Entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long>{
-
-}
